a4d1dd5a1866841227a5ef3473b1e40c0617fa3a
kvanzuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2024-01-11 22:27:20 +0100

    ci: Added error handling for logger sync

--- main.go
+++ main.go
@@ -24,7 +24,12 @@ func createLogger() *zap.Logger {
 func init() {
 	logger := createLogger()
 
-	defer logger.Sync()
+	defer func(logger *zap.Logger) {
+		err := logger.Sync()
+		if err != nil {
+			panic(err)
+		}
+	}(logger)
 
 	zap.ReplaceGlobals(logger)
 
